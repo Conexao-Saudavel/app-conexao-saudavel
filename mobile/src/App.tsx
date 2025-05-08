eb41dfde9a613576ce31458362b1c4305c980eec
Francisco Macedo <macedo270701@gmail.com>
2025-05-08 19:19:01 -0300

    feat: Integrates navigation and theming support
    Wraps the main navigator with a navigation container and paper provider, replacing basic layout components to support consistent theming.

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,12 +1,20 @@
+// App.tsx
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+// Importe seus navegadores aqui
+import MainNavigator from './navigation/MainNavigator'; // Exemplo
+import { paperTheme } from './theme/paperTheme';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle="dark-content" />
-    </SafeAreaView>
+    <PaperProvider theme={paperTheme}>
+      <NavigationContainer>
+        {/* Seus navegadores, ex: <AuthNavigator /> ou <MainNavigator /> */}
+        <MainNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
-export default App; 
+export default App;
