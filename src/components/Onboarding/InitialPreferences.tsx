24ed321ee77910544110a78cc66c97450a862175
Francisco Macedo <macedo270701@gmail.com>
2025-06-12 12:48:32 -0300

    feat: initiate screen time monitoring upon completion of preferences

--- src/components/Onboarding/InitialPreferences.tsx
+++ src/components/Onboarding/InitialPreferences.tsx
@@ -36,6 +36,7 @@ export const InitialPreferences: React.FC<InitialPreferencesProps> = ({
       return;
     }
 
+    await screenTimeService.startMonitoring();
     onComplete({
       screenTimeGoal,
       notificationsEnabled,
